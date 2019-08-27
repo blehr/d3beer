b4f7ec3cd0164a63aee412320650e1898d832961
brandonlehr <blehr.mail@gmail.com>
2019-08-27 10:45:36 -0400

    Updates

---
+++ precache-manifest.359c26f3f022401a17764c3ee4502fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673a7c99f38c85d9d5e0e1393e5d7fb2",
+    "url": "/d3-beer/index.html"
+  },
+  {
+    "revision": "b8d85721f8ba2ed4712a",
+    "url": "/d3-beer/static/css/main.d45fec36.chunk.css"
+  },
+  {
+    "revision": "b6e3c805b9b3df2679ee",
+    "url": "/d3-beer/static/js/2.b7f8d678.chunk.js"
+  },
+  {
+    "revision": "b8d85721f8ba2ed4712a",
+    "url": "/d3-beer/static/js/main.4bbba5f3.chunk.js"
+  },
+  {
+    "revision": "fd0d26ef745904c6828e",
+    "url": "/d3-beer/static/js/runtime~main.24159cc3.js"
+  },
+  {
+    "revision": "a2067fe0f9373f581223ca79b18d8c8f",
+    "url": "/d3-beer/static/media/states.a2067fe0.csv"
+  }
+]);
